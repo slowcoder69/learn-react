f777801f898755cfebeb678a5768a8b30dc12ebe
Slow Coder <slowcoder@gmail.com>
2023-10-22 12:16:30 +0700

    finish tic tac toe challenge

--- src/App.jsx
+++ src/App.jsx
@@ -1,71 +1,96 @@
 import { useState } from 'react'
 import './App.css'
 
-function Square({ value, onClick }) {
+function Square({ win, value, onClick }) {
   return (
-    <button className='square' onClick={onClick}>{value}</button>
+    <button className={`square ${win ? "win" : ""}`} onClick={onClick}>{value}</button>
   )
 }
 
 function Board({ squares, xIsNext, onSquareClick }) {
-  const winner = calculateWinner(squares)
+  const end = squares.every((i) => i)
+  const result = calculateWinner(squares)
+  const info = !!result.length
+    ? `Winner: ${xIsNext ? "O" : "X"}`
+    : !end ? `Next player: ${xIsNext ? "X" : "O"}` : `Draw!!`
 
-  let info = ""
-  if (winner) {
-    info = "Winner: " + winner
-  } else if (xIsNext) {
-    info = "Next player: X"
-  } else {
-    info = "Next player: O"
+  const boardRows = []
+  for (let i = 0; i < 3; i++) {
+    const boardColumns = []
+    for (let j = 0; j < 3; j++) {
+      const idx = 3 * i + j
+      boardColumns[j] = (
+        <Square
+          key={j}
+          win={result.includes(idx)}
+          value={squares[idx]}
+          onClick={() => onSquareClick(idx, [i, j])}
+        />
+      )
+    }
+    boardRows[i] = <div key={i} className='board-row'>{boardColumns}</div>
   }
 
   return (
     <div className='board'>
-      <div className='board-row'>
-        <Square value={squares[0]} onClick={() => onSquareClick(0)} />
-        <Square value={squares[1]} onClick={() => onSquareClick(1)} />
-        <Square value={squares[2]} onClick={() => onSquareClick(2)} />
-      </div>
-      <div className='board-row'>
-        <Square value={squares[3]} onClick={() => onSquareClick(3)} />
-        <Square value={squares[4]} onClick={() => onSquareClick(4)} />
-        <Square value={squares[5]} onClick={() => onSquareClick(5)} />
-      </div>
-      <div className='board-row'>
-        <Square value={squares[6]} onClick={() => onSquareClick(6)} />
-        <Square value={squares[7]} onClick={() => onSquareClick(7)} />
-        <Square value={squares[8]} onClick={() => onSquareClick(8)} />
-      </div>
+      {boardRows}
       <div>{info}</div>
     </div>
   )
 }
 
 function History({ histories, onItemClick }) {
+  const [sortAsc, setSortAsc] = useState(true)
+
+  function handleSortClick() {
+    setSortAsc(!sortAsc)
+  }
+
+  const historiesComponents = histories.map((item, i) => {
+    if (histories > 1 && i === histories.length - 1) return
+
+    return (
+      <button key={i} className='history-item' onClick={() => onItemClick(i)}>
+        {i === 0 ? "Go to game start" : `Go to move #${i} (${item.move.join(",")})`}
+      </button>
+    )
+  })
+
+  if (!sortAsc) {
+    historiesComponents.reverse()
+  }
+
   return (
     <div className='histories'>
-      {histories.map((_, i) => (
-        <button key={i} className='history-item' onClick={() => onItemClick(i)}>
-          #{i + 1}
-        </button>
-      ))}
+      <button className='history-item' onClick={handleSortClick}>
+        Sort: {sortAsc ? "ASC" : "DESC"}
+      </button>
+      {historiesComponents}
+      <div className='history-label'>
+        You are at move #{histories.length - 1}
+      </div>
     </div>
   )
 }
 
 function App() {
-  const [histories, setHistories] = useState([Array(9).fill("")])
+  const [histories, setHistories] = useState([
+    { squares: Array(9).fill(""), move: [] }
+  ])
   const [currentHistory, setCurrentHistory] = useState(0)
 
-  const squares = histories[currentHistory]
+  const squares = histories[currentHistory].squares
 
-  function handleSquareClick(index) {
-    if (squares[index] || calculateWinner(squares)) return
+  function handleSquareClick(index, move) {
+    if (squares[index] || !!calculateWinner(squares).length) return
 
-    const newHistory = squares.slice()
-    newHistory[index] = xIsNext ? "X" : "O"
+    const newSquares = squares.slice()
+    newSquares[index] = xIsNext ? "X" : "O"
 
-    const newHistories = [...histories.slice(0, currentHistory + 1), newHistory]
+    const newHistories = [
+      ...histories.slice(0, currentHistory + 1),
+      { squares: newSquares, move }
+    ]
 
     setHistories(newHistories)
     setCurrentHistory(currentHistory + 1)
@@ -100,13 +125,13 @@ function calculateWinner(squares) {
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i]
     if (squares[a] && squares[a] === squares[b] && squares[b] === squares[c]) {
-      return squares[a]
+      return lines[i]
     }
   }
 
-  return ""
+  return []
 }
 
 export default App
 
-// todo: tic tac toe improvement from react docs
+// todo: continue react docs (thinking in react)
