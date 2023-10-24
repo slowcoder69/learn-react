885e4215e451a01334636eb028d07e25ffd2f44d
Slow Coder <slowcoder@gmail.com>
2023-10-24 09:59:30 +0700

    setup linter and typescript

--- src/TicTacToe.jsx
+++ src/TicTacToe.jsx
@@ -1,24 +1,34 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import PropTypes from "prop-types";
 
 function Square({ win, value, onClick }) {
   return (
-    <button className={`square ${win ? "win" : ""}`} onClick={onClick}>{value}</button>
-  )
+    <button className={`square ${win ? "win" : ""}`} onClick={onClick}>
+      {value}
+    </button>
+  );
 }
+Square.propTypes = {
+  win: PropTypes.bool.isRequired,
+  value: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
 
 function Board({ squares, xIsNext, onSquareClick }) {
-  const end = squares.every((i) => i)
-  const result = calculateWinner(squares)
-  const info = !!result.length
+  const end = squares.every((i) => i);
+  const result = calculateWinner(squares);
+  const info = result.length
     ? `Winner: ${xIsNext ? "O" : "X"}`
-    : !end ? `Next player: ${xIsNext ? "X" : "O"}` : `Draw!!`
+    : !end
+    ? `Next player: ${xIsNext ? "X" : "O"}`
+    : `Draw!!`;
 
-  const boardRows = []
+  const boardRows = [];
   for (let i = 0; i < 3; i++) {
-    const boardColumns = []
+    const boardColumns = [];
     for (let j = 0; j < 3; j++) {
-      const idx = 3 * i + j
+      const idx = 3 * i + j;
       boardColumns[j] = (
         <Square
           key={j}
@@ -26,88 +36,107 @@ function Board({ squares, xIsNext, onSquareClick }) {
           value={squares[idx]}
           onClick={() => onSquareClick(idx, [i, j])}
         />
-      )
+      );
     }
-    boardRows[i] = <div key={i} className='board-row'>{boardColumns}</div>
+    boardRows[i] = (
+      <div key={i} className="board-row">
+        {boardColumns}
+      </div>
+    );
   }
 
   return (
-    <div className='board'>
+    <div className="board">
       {boardRows}
       <div>{info}</div>
     </div>
-  )
+  );
 }
+Board.propTypes = {
+  squares: PropTypes.array.isRequired,
+  xIsNext: PropTypes.bool.isRequired,
+  onSquareClick: PropTypes.func.isRequired,
+};
 
 function History({ histories, onItemClick }) {
-  const [sortAsc, setSortAsc] = useState(true)
+  const [sortAsc, setSortAsc] = useState(true);
 
   function handleSortClick() {
-    setSortAsc(!sortAsc)
+    setSortAsc(!sortAsc);
   }
 
   const historiesComponents = histories.map((item, i) => {
-    if (histories > 1 && i === histories.length - 1) return
+    if (histories > 1 && i === histories.length - 1) return;
 
     return (
-      <button key={i} className='history-item' onClick={() => onItemClick(i)}>
-        {i === 0 ? "Go to game start" : `Go to move #${i} (${item.move.join(",")})`}
+      <button key={i} className="history-item" onClick={() => onItemClick(i)}>
+        {i === 0
+          ? "Go to game start"
+          : `Go to move #${i} (${item.move.join(",")})`}
       </button>
-    )
-  })
+    );
+  });
 
   if (!sortAsc) {
-    historiesComponents.reverse()
+    historiesComponents.reverse();
   }
 
   return (
-    <div className='histories'>
-      <button className='history-item' onClick={handleSortClick}>
+    <div className="histories">
+      <button className="history-item" onClick={handleSortClick}>
         Sort: {sortAsc ? "ASC" : "DESC"}
       </button>
       {historiesComponents}
-      <div className='history-label'>
+      <div className="history-label">
         You are at move #{histories.length - 1}
       </div>
     </div>
-  )
+  );
 }
+History.propTypes = {
+  histories: PropTypes.array.isRequired,
+  onItemClick: PropTypes.func.isRequired,
+};
 
 function App() {
   const [histories, setHistories] = useState([
-    { squares: Array(9).fill(""), move: [] }
-  ])
-  const [currentHistory, setCurrentHistory] = useState(0)
+    { squares: Array(9).fill(""), move: [] },
+  ]);
+  const [currentHistory, setCurrentHistory] = useState(0);
 
-  const squares = histories[currentHistory].squares
+  const squares = histories[currentHistory].squares;
 
   function handleSquareClick(index, move) {
-    if (squares[index] || !!calculateWinner(squares).length) return
+    if (squares[index] || !!calculateWinner(squares).length) return;
 
-    const newSquares = squares.slice()
-    newSquares[index] = xIsNext ? "X" : "O"
+    const newSquares = squares.slice();
+    newSquares[index] = xIsNext ? "X" : "O";
 
     const newHistories = [
       ...histories.slice(0, currentHistory + 1),
-      { squares: newSquares, move }
-    ]
+      { squares: newSquares, move },
+    ];
 
-    setHistories(newHistories)
-    setCurrentHistory(currentHistory + 1)
+    setHistories(newHistories);
+    setCurrentHistory(currentHistory + 1);
   }
 
   function handleHistoryClick(index) {
-    setCurrentHistory(index)
+    setCurrentHistory(index);
   }
 
-  const xIsNext = currentHistory % 2 === 0
+  const xIsNext = currentHistory % 2 === 0;
 
   return (
-    <div className='game'>
-      <Board xIsNext={xIsNext} squares={squares} onSquareClick={handleSquareClick} />
+    <div className="game">
+      <Board
+        xIsNext={xIsNext}
+        squares={squares}
+        onSquareClick={handleSquareClick}
+      />
       <History histories={histories} onItemClick={handleHistoryClick} />
     </div>
-  )
+  );
 }
 
 function calculateWinner(squares) {
@@ -120,18 +149,18 @@ function calculateWinner(squares) {
     [2, 5, 8],
     [0, 4, 8],
     [2, 4, 6],
-  ]
+  ];
 
   for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i]
+    const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[b] === squares[c]) {
-      return lines[i]
+      return lines[i];
     }
   }
 
-  return []
+  return [];
 }
 
-export default App
+export default App;
 
-// todo: continue react docs (thinking in react)
+// convert to typescript later
